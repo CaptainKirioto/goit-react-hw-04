6f5f8b218fad8d90f309df4353321650aec8c487
CaptainKirioto <mrg.zhi@gmail.com>
2024-08-04 16:35:51 +0200

    Api stuff

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_KEY = "727gOy69tyeyfXPWewhpw1BCd94fcx4353H_X08pkK4";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 axios.defaults.headers.common["Authorization"] = `Client-ID ${API_KEY}`;
